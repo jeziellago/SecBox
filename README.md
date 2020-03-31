0d42cce7e289faface2856863ff7060c185f0abc
Jeziel Lago <jeziellago@gmail.com>
2020-03-31 17:42:22 -0300

    Reordering files

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # SecBox
 Script Box for Android Reverse Engineering
+
+```
+        Welcome to
+        _________            ___.                 
+        /   _____/ ____   ____\_ |__   _______  ___
+        \_____  \_/ __ \_/ ___\| __ \ /  _ \  \/  /
+        /        \  ___/\  \___| \_\ (  <_> >    < 
+        /_______  /\___  >\___  >___  /\____/__/\_ \\
+                \/     \/     \/    \/            \/
+                                powered by @jeziellago
+```
+
+### Run
+```
+$ cd secbox/
+$ python3 main.py
+```
+
+**This tool is working in progress**
--- secbox/apktool_wrapper.py
+++ secbox/apktool_wrapper.py
@@ -2,10 +2,10 @@ from os import system, path
 
 # ------------------------------------------------------- #
 def decompile(apk_path, options, target_dir):
-    system('java -jar apktool.jar d %s %s -o %s' % (options, apk_path, target_dir))
+    system('java -jar tool/apktool.jar d %s %s -o %s' % (options, apk_path, target_dir))
 # ------------------------------------------------------- #
 def recompile(source_dir, target_apk):
     if path.isdir(source_dir):
-        system('java -jar apktool.jar b %s -o %s' % (source_dir, target_apk))
+        system('java -jar tool/apktool.jar b %s -o %s' % (source_dir, target_apk))
     else:
         print('%s is an invalid dir.' % source_dir)
