78c1feb577eb644036317ee40150fb91365ab440
Jeziel Lago <jeziellago@gmail.com>
2021-12-01 12:47:21 -0300

    Fix apktool path

--- secbox/reveng/apktool_wrapper.py
+++ secbox/reveng/apktool_wrapper.py
@@ -2,12 +2,12 @@ from os import system, path
 
 
 def decompile(apk_path, options, target_dir):
-    command_template = 'java -jar tool/apktool.jar d %s %s -o %s --use-aapt2'
+    command_template = 'java -jar ${HOME}/secbx/tool/apktool.jar d %s %s -o %s --use-aapt2'
     proc = system(command_template % (options, apk_path, target_dir))
     return proc == 0
 
 def recompile(source_dir, target_apk):
-    command_template = 'java -jar tool/apktool.jar b %s -o %s --use-aapt2'
+    command_template = 'java -jar ${HOME}/secbx/tool/apktool.jar b %s -o %s --use-aapt2'
     proc = system(command_template % (source_dir, target_apk))
     return proc == 0
     
